b71a382c47403c4a6b2630c5568ffc6a71cc6f2a
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2023-09-06 15:14:47 +0300

    🎨 CSS modifications

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,13 +1,20 @@
 //it just display what percent user packed from his list
 const Stats = ({ items, numPacked }) => {
   const numItems = items.length;
-
   const percentage = Math.round((numPacked / numItems) * 100);
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding your list 🚀</em>
+      </p>
+    );
   return (
     <footer className="stats">
       <em>
-        You have {numItems} items in your list, you packed {numPacked} from list
-        which is ({percentage}%)
+        {percentage === 100
+          ? "You're ready to go now, have a safe flight ✈️"
+          : `You have ${numItems} items in your list, you packed ${numPacked} from list
+        which is (${percentage}%)`}
       </em>
     </footer>
   );
